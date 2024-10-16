3cd9ce80b089de350b882299ee1078a913481371
vimbainashe mapundu <vimbainashetrinity229@gmail.com>
2024-10-16 02:04:23 +0100

    initial commit

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -9,8 +9,10 @@ export async function updateSession(request: NextRequest) {
     })
 
     const supabase = createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+
+
+        process.env.NEXT_PUBLIC_SUPABASE_URL!!,//oghpmdquumycxxkkjqcb.supabase.co,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,//eyJJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9naHBtZHF1dW15Y3h4a2tqcWNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA0OTQyMjQsImV4cCI6MjAzNjA3MDIyNH0.bKJHMadnvVhQ7nPdiujVblywzkyN5K7U4X-uyRshoCU
         {
             cookies: {
                 get(name: string) {
